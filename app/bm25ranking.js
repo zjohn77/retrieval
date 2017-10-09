1c8be051ec8b6171c2fc69440ab0c9538b6f2a54
John Jung <zjohn77@gmail.com>
2017-10-09 12:49:16 -0400

    To put together a corpus for unit testing.

--- app/bm25ranking.js
+++ app/bm25ranking.js
@@ -4,6 +4,7 @@
 const chunkFilterStem = require("./lib/NLProc/app/chunk_filter_stem.js");
 const bm25 = require("./lib/bm25/app/controller/bm25.js");
 
-titleUrls.map(chunkFilterStem);
 
-var docTermMatr = bm25(titleTokensMatr);// computes the matrix of BM25 weights
+// titleUrls.map(chunkFilterStem);
+//
+// var docTermMatr = bm25(titleTokensMatr);// computes the matrix of BM25 weights
