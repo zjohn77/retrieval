a98461ad4c90fbf005cb39fe1e4b09e7e0669a5b
John Jung <zjohn77@gmail.com>
2017-10-10 20:30:43 -0400

    make loop more readable via lodash's foreach method

--- app/bm25ranking.js
+++ app/bm25ranking.js
@@ -1,10 +1,21 @@
 /*
 * Vectorize the NLP/chunked/lemmatized texts to matrix of BM25 weights.
 */
-const processCorpus = require("./model/proc_corpus.js");
-const Bm25 = require("../lib/bm25");
+const applyNLP = require("../lib/NLProc"); // fcn that applies NLP to a str
+const Bm25 = require("../lib/bm25"); // class definition
 
-console.log(nlprocess('John is a data scientist who happens to like coffee.'));
+module.exports = function(docs){
+  // Applies NLProc to each document in an array of documents (a.k.a. corpus).
+  let bagOfWords = [];
+  for(let i = 0; i < docs.length; i++)
+  {
+    bagOfWords.push(applyNLP(docs[i]));
+  }
+  // APPLY LODASH'S _FOREACH HERE.
 
+  Bm25(bagOfWords);
+};
+
+// console.log(nlprocess('John is a data scientist who happens to like coffee.'));
 // titleUrls.map(chunkFilterStem);
 // var docTermMatr = bm25(titleTokensMatr);// computes the matrix of BM25 weights
