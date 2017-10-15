045db649200491ceeaac44c2fd32eb3c6efc1cac
John Jung <zjohn77@gmail.com>
2017-10-14 22:53:27 -0400

    to run the unit test for Bm25

--- app/bm25ranking.js
+++ app/bm25ranking.js
@@ -1,20 +1,23 @@
 /*
 * Vectorize the NLP/chunked/lemmatized texts to matrix of BM25 weights.
 */
-const applyNLP = require("../lib/NLProc"); // fcn that applies NLP to a str
-const Bm25 = require("../lib/bm25"); // class definition
+// const applyNLP = require("../node_modules/NLProc"); // fcn that applies NLP to a str
+// const Bm25 = require("../node_modules/bm25"); // class definition
+const Bm25 = require("bm25"); // class definition
 
-module.exports = function(docs){
-  // Applies NLProc to each document in an array of documents (a.k.a. corpus).
-  let bagOfWords = [];
-  for(let i = 0; i < docs.length; i++)
-  {
-    bagOfWords.push(applyNLP(docs[i]));
-  }
-  // APPLY LODASH'S _FOREACH HERE.
+Bm25([]);
 
-  Bm25(bagOfWords);
-};
+// module.exports = function(docs){
+//   // Applies NLProc to each document in an array of documents (a.k.a. corpus).
+//   let bagOfWords = [];
+//   for(let i = 0; i < docs.length; i++)
+//   {
+//     bagOfWords.push(applyNLP(docs[i]));
+//   }
+//   // APPLY LODASH'S _FOREACH HERE.
+//
+//   Bm25(bagOfWords);
+// };
 
 // console.log(nlprocess('John is a data scientist who happens to like coffee.'));
 // titleUrls.map(chunkFilterStem);
