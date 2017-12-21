e93620f48a51bec5e1e01f2aa56d8a4c222af37c
John Jung <zjohn77@gmail.com>
2017-12-20 22:28:16 -0500

    to upgrade Node from v6.8.1 to v7 so that Object.values() will work

--- src/score_selection/top_indices.js
+++ src/score_selection/top_indices.js
@@ -2,12 +2,11 @@
  * Use the heap to select the indices for the 10 highest bm25 scores in
  * O(N * log(10)) computations.
  */
-const sortKeys = require('sort-keys');
 const nArgmax = require('./lib/n_argmax.js');
+const sortKeys = require('sort-keys');
 
-module.exports = function(arr) {
-    let getTopIndices = _.flow([nArgmax, sortKeys, Object.values]);
-    return [].concat(...getTopIndices(arr)
-                        .reverse()
+module.exports = function(arr, n=10) {
+    let sortedObj = sortKeys(nArgmax(arr, n));
+    return [].concat(...Object.values(sortedObj).reverse()
                     );
 };
--- test/test_scr_select.js
+++ test/test_scr_select.js
@@ -1,21 +1,22 @@
 const assert = require('assert');
-const nArgmax = require('../src/util/n_argmax.js');
-const topIndices = require('./score_selection/top_indices.js');
+const nArgmax = require('../src/score_selection/lib/n_argmax.js');
+const topIndices = require('../src/score_selection/top_indices.js');
 
 describe('Test all fcns in the util folder', function() {
     before(function() {
+        ARR = [70, 6, 6, 4];
+        TOP_N = 2;
     });
 
-    it('1. nArgmax should find the n-biggest elements & their indices', function(){
+    it('nArgmax should find the n-biggest elements & their indices',
+      function() {
         let argmaxObj = { '6': ['1', '2'],
                          '70': ['0'] };
-        assert.deepEqual(argmaxObj, nArgmax([70, 6, 6, 4], n=2));
+        assert.deepEqual(argmaxObj, nArgmax(ARR, TOP_N));
     });
 
-    it('2. topIndices should find the n-biggest elements & their indices', function(){
-        let argmaxObj = { '6': ['1', '2'],
-                         '70': ['0'] };
-        assert.deepEqual(argmaxObj, nArgmax([70, 6, 6, 4], n=2));
+    it('topIndices should find the indices of the n-biggest elements',
+      function() {
+        assert.deepEqual([['0'], ['1', '2']], topIndices(ARR, TOP_N));
     });
-
 });
