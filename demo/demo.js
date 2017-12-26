e9a487c6889ea1fd1010127c7007452e8a9de165
John Jung <zjohn77@gmail.com>
2017-12-25 22:14:00 -0500

    to complete the readme

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,11 @@
-const docs = require("./music-collection");
-const Retrieval = require("../src/Retrieval.js");
+const docs = require("./music-collection"); //load sample documents
+const Retrieval = require("../../retrieval"); //import module
 
-let rt = new Retrieval();
+//1st step: construct an object, feeding two parameters for bm25.
+let rt = new Retrieval(K=2, B=0.75);
+
+//2nd step: index the document collection loaded above.
 rt.index(docs);
-console.log(rt.search());
+
+//3rd step: search.
+rt.search('theme and variations');
--- test/test_Bm25.js
+++ test/test_Bm25.js
@@ -1,48 +1,80 @@
 var assert = require('assert');
+var uniqTerms = require("../src/Bm25/idf_map/lib/vocab.js");
+var idfMap = require("../src/Bm25/idf_map/idf_map.js");
 var docLen = require("../src/Bm25/vec_space/lib/doc_len.js");
 var vsm = require("../src/Bm25/vec_space/vec_space.js");
 var Bm25 = require("../src/Bm25/Bm25.js");
 
-describe('Using the same corpus, test all essential fcns of the BM25 app.', function() {
+describe('Using the same corpus, test all essential fcns of the BM25 app.',
+  function() {
     before(function() {
-      // Define a two-document sample corpus.
-      sampMatr = [['uk','gramophone','gramophone','discussion'],
-      				    ['gramophone','into','general','discussion','into','into']];
+        // A two-document corpus to test idf_map functions.
+        corpus = [ {"over":1,
+                    "great":1,
+                    "Buck":1,
+                    "ruled":1},
+                   {"Buck":1,
+                    "house":1,
+                    "dog":2,
+                    "kennel":1} ];
 
-      // What the outcome document dicts will look like for this sample corpus.
-      var doc1 = {'uk': 1,
-                  'gramophone': 2,
-                  'discussion': 1};
-      var doc2 = {'gramophone': 1,
-                  'into': 3,
-                  'general': 1,
-                  'discussion': 1};
-      vsmObj = {'docLens': [0.8, 1.2],
-                'docs': [doc1, doc2]};
-
-      // The resulting BM25 weighted doc-term matrix for the above sample corpus.
-      bm = new Bm25(sampMatr);
-      bm25matr = [ [  0.42372881355932196,
-                      0.3538898166022831,
-                      0.25192156436094726,
-                      0,
-                      0 ],
-                    [ 0,
-                      0.20934327179289988,
-                      0.20934327179289988,
-                      0.6198347107438017,
-                      0.35211267605633806 ] ];
-    });//closing parenthesis of the before fcn
-
-    it('should compute vector of relative document lengths for each doc in corpus', function(){
-      assert.deepStrictEqual([0.8, 1.2], docLen(sampMatr));
+        // A two-document corpus to test vec_space functions.
+        sampMatr = [['uk','gramophone','gramophone','discussion'],
+        				    ['gramophone','into','general','discussion','into','into']];
+        let _doc1 = {'uk': 1,
+                     'gramophone': 2,
+                     'discussion': 1};
+        let _doc2 = {'gramophone': 1,
+                     'into': 3,
+                     'general': 1,
+                     'discussion': 1};
+        vsmObj = {'docLens': [0.8, 1.2],
+                  'docs': [_doc1, _doc2]};
     });
 
-    it('should compute a Vector Space Object from a corpus', function(){
-      assert.deepStrictEqual(vsmObj, vsm(sampMatr));
+    it('1. should compute the vocabulary set of a document collection',
+      function() {
+        let vocabulary = new Set(['over', 'great', 'Buck', 'ruled', 'house', 'dog', 'kennel']);
+        assert.deepStrictEqual(vocabulary,
+                               uniqTerms(corpus.map(Object.keys, Object)));
     });
 
-    it('should build the BM25 doc-term matrix of that corpus', function(){
-      assert.deepStrictEqual(bm25matr, bm.buildMatr());
+    it('2. should compute the IDF of each word in that vocabulary',
+      function() {
+        let idf = {'over':1,
+                   'great':1,
+                   'Buck':0.5945348918918356,
+                   'ruled':1,
+                   'house':1,
+                   'dog':1,
+                   'kennel':1};
+        assert.deepStrictEqual(idf, idfMap(corpus));
+    });
+
+    it('3. should compute vector of relative document lengths for each doc in corpus',
+      function() {
+        assert.deepStrictEqual([0.8, 1.2], docLen(sampMatr));
+    });
+
+    it('4. should compute a Vector Space Object from a document collection',
+      function() {
+        assert.deepStrictEqual(vsmObj, vsm(sampMatr));
+    });
+
+    it('5. should build the BM25 matrix based on a document collection',
+      function() {
+        // The resulting BM25 weighted doc-term matrix for the above sample corpus.
+        let bm = new Bm25(sampMatr);
+        let bm25matr = [ [  0.42372881355932196,
+                            0.3538898166022831,
+                            0.25192156436094726,
+                            0,
+                            0 ],
+                          [ 0,
+                            0.20934327179289988,
+                            0.20934327179289988,
+                            0.6198347107438017,
+                            0.35211267605633806 ] ];
+        assert.deepStrictEqual(bm25matr, bm.buildMatr());
     });
 });
